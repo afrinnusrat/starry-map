811c5079aa318feb70c9abb0140064ac1a37170a
Kathy Lu <kaffy.lu@gmail.com>
2016-01-07 13:31:32 -0500

    initial commit!

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var sass = require('node-sass-middleware');
+var swig = require('swig');
+var routes = require('./routes/index');
+
+var app = express();
+
+app.engine('html', swig.renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(
+ sass({
+   src: __dirname + '/assets', //where the sass files are
+   dest: __dirname + '/public', //where css should go
+   debug: true
+ })
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+//development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var server = app.listen(3000, function()
+{
+  console.log("listening on 3000");
+});
+module.exports = app;
